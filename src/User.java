e0d1b9a7b301761fe992e5aa8627eb5b7a5bd2f6
edensaar1 <eden2000yo@gmail.com>
2026-03-25 23:00:05 +0200

    email regex correct

--- src/User.java
+++ src/User.java
@@ -1,2 +1,26 @@
 public class User {
+    String username;
+    String password;
+
+    public User(String username, String password) throws Exception{
+        if(username.length() > 50){
+            throw new Exception("Username is too long, try something shorter");
+        }
+        else if(!username.matches("^[A-Za-z0-9][A-Za-z0-9._%+-]+@[A-Za-z0-9][A-Za-z0-9.-]*\\.[A-Za-z]{2,}$")){
+           throw new Exception("Please enter a valid Email as username");
+        }
+
+        if(password.length() < 8){
+           throw new Exception("Your password is too short, add more characters");
+        }
+        else if(password.length() > 12){
+            throw new Exception("Your password is too long, try a shorter one");
+        }
+        else if(!password.matches("^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@#!_,.+])[A-Za-z0-9@#!_,.+]+$")){
+            throw new Exception("Please enter a valid password");
+        }
+
+        this.username = username;
+        this.password = password;
+    }
 }
